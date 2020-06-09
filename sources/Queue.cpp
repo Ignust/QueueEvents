e95af9334b7deb01082cba7561e0a39be3e34cb8
Ignust <andrey.las90@gmail.com>
2020-06-10 00:01:26 +0300

    added -pthread in CMake

--- sources/Queue.cpp
+++ sources/Queue.cpp
@@ -46,8 +46,8 @@ void Queue::run()
 void Queue::processEvent(tEvent event)
 //------------------------------------------------------------------------------------------
 {
-    //std::thread th([&](){mA->receiveEvent(event.fancID_, event.buffer);});
-    //th.detach();
+    std::thread th([&](){mA->receiveEvent(event.fancID_, event.buffer);});
+    th.detach();
     //std:: thread th2 ([](){});
     //pthread_create([](){});
     return;
